9d1e90816df8c7a8537dd9f5100dd423594425ce
helena karol <helenadiasmaia@gmail.com>
2021-04-02 14:53:41 -0300

    video aula 2

---
+++ index.js
@@ -0,0 +1,19 @@
+
+//console.log(Document);
+
+const botaoTeste = document.getElementById('botao-teste');
+const outroBotao = document.getElementById('outro-botao');
+
+console.log(botaoTeste);
+console.log(outroBotao);
+
+const botaoSelector = document.querySelector('#botao-selector');
+
+console.log(botaoSelector);
+
+const botaoPrincipal = document.querySelector('.botao-principal');
+
+console.log(botaoPrincipal);
+
+const lista = document.querySelector('ul');
+console.log(lista);
